46c91c88fd11bc457f0c7cc8aa68aeb81b7d6006
Fred <thecanuckatheist@gmail.com>
2019-10-24 20:11:40 +0000

    cats and more cats, with tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
-const breed = process.argv[2];
 
-console.log(breed);
-let address = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
+const fetchBreedDescription = function(breedName, callback) {
+  let address = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(address, (error, response, body) => {
+    let newBody = JSON.parse(body);
+    let desc = newBody[0].description.trim();
+    callback(error, desc);
+  });
+};
+// request(address, (error, response, body) => {
+//   console.log(response.statusCode);
+//   let newBody = JSON.parse(body);
+//   if (error) {
+//     console.log("There was an error when trying to search for " + breed);
+//   } else if (JSON.stringify(newBody) === '[]') {
+//     console.log("The particular breed " + breed + " could not be found.");
+//   } else {
+//     console.log(newBody);
+//     console.log(newBody[0].name);
+//   }
+// });
 
-request(address, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  //console.log(typeof body);
-  console.log(response.statusCode);
-  let newBody = JSON.parse(body);
-  if (error) {
-    console.log("There was an error when trying to search for " + breed);
-  } else if (JSON.stringify(newBody) === '[]') {
-    console.log("The particular breed " + breed + " could not be found.");
-  } else {
-    console.log(newBody);
-    console.log(newBody[0].name);
-  }
-});
+module.exports = { fetchBreedDescription };
